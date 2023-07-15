6e93f4aa40e86ef514fb72e95bc171cf0b912da5
Intikhab <islaminitkhab2gmail.com>
2023-07-15 23:12:54 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import FormValidation from "./Components/FormValidation";
+import "./style.css";
+
+
+const App = () => {
+
+    return(
+        <div>
+           
+            <FormValidation />
+        </div>
+    )
+}
+
+export default App;
